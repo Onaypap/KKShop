e9305023aac1306ec7a95401ed37ac0ad8c797cd
Onaypap <rominougonone@gmail.com>
2025-06-28 18:50:31 +0200

    Add files via upload

---
+++ products-data.js
@@ -0,0 +1,64 @@
+// products-data.js
+
+const products = [
+  {
+    name: "Pâtes Penne",
+    description: "Marque: Panzati",
+    price: 6.82,
+    image: "images/tomatetesco.png"
+  },
+  {
+    name: "Bouteille d'eau",
+    description: "Marque: Cristal",
+    price: 4.49,
+    image: "images/littesco.png"
+  },
+  {
+    name: "Céréals au miel",
+    description: "Marque: Honey Bees",
+    price: 12.63,
+    image: "images/papiertoilettetesco.png"
+  },
+  {
+    name: "Riz",
+    description: "Marque: Elo",
+    price: 7.32,
+    image: "nectarinetesco.png"
+  },
+  {
+    name: "Sel",
+    description: "Marque: Lerequin",
+    price: 2.27,
+    image: "images/viandetesco.png"
+  },
+  {
+    name: "Sucre",
+    description: "Marque: Daron",
+    price: 7.53,
+    image: "images/lunettes-soleil.png"
+  },
+  {
+    name: "Margarine",
+    description: "Marque: Etron",
+    price: 9.97,
+    image: "images/chino-beige.png"
+  },
+  {
+    name: "Farine",
+    description: "Marque: Kraftung",
+    price: 4.27,
+    image: "images/sweat-gris.png"
+  },
+  {
+    name: "Jus de Pomme",
+    description: "Marque: Bio",
+    price: 8.78,
+    image: "images/baskets-blanches.png"
+  },
+  {
+    name: "Huile d'olive",
+    description: "Marque: Tupiges",
+    price: 36.19,
+    image: "images/parka-impermeable.png"
+  }
+];
---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript pour une interaction simple
+document.getElementById("action").addEventListener("click", function() {
+  document.getElementById("message").textContent = "Bravo ! Tu as cliqué sur le bouton 🎉";
+});
